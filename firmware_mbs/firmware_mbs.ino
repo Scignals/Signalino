59b128a14d2988f87b1834aed44b26318ab95734
jabh <jbarios@umh.es>
2016-11-27 22:26:19 +0100

    añadida licencia gpl v

--- firmware_mbs/firmware_mbs.ino
+++ firmware_mbs/firmware_mbs.ino
@@ -1,8 +1,23 @@
-// signalino, firmware v 02 rama master
-// firmware for controlling the ADS1299-based signalino
-// Created: JABarios, agosto 2016 - enero 2017
-// No warranty.  Use at your own risk.  Use for whatever you'd like.
+/* 
+* signalino, firmware v 02 rama master
+* firmware for controlling the ADS1299-based signalino
+* Author: JABarios, agosto 2016 - enero 2017
+* Company: ILSB, Spain. 
+* No warranty.  Use at your own risk.  Use for whatever you'd like.
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include <stdlib.h>     /* strtoul */
 #include <SPI.h>  // include the SPI library:
--- mbs_pc/mbs_pc.pde
+++ mbs_pc/mbs_pc.pde
@@ -9,6 +9,26 @@
 //
 // No warranty.  Use at your own risk.  Use for whatever you'd like.
 // 
+
+/*
+* Author: JABarios, agosto 2016 - enero 2017
+* Company: ILSB, Spain. 
+* No warranty.  Use at your own risk.  Use for whatever you'd like.
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 ///////////////////////////////////////////////
 
 import controlP5.*;
